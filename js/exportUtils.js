c85c1c842e443011809cabdd3604dbd9f9e4ea0a
SageGallant <ramchouhan160@gmail.com>
2025-03-07 18:43:52 +0530

    Add Excel and PDF export functionality

--- js/exportUtils.js
+++ js/exportUtils.js
@@ -52,3 +52,51 @@ class ExportUtils {
 
 // Export instance
 const exportUtils = new ExportUtils();
+/**
+ * Export aliases as Excel file (XLSX)
+ */
+ExportUtils.prototype.exportAsExcel = function(aliases) {
+  // Create worksheet
+  const ws = XLSX.utils.aoa_to_sheet([["Aliases"], ...aliases.map(a => [a])]);
+  
+  // Create workbook
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Email Aliases");
+  
+  // Generate and download
+  XLSX.writeFile(wb, `${this.filenameBase}.xlsx`);
+};
+
+/**
+ * Export aliases as PDF file
+ */
+ExportUtils.prototype.exportAsPDF = function(aliases) {
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF();
+  
+  // Add title
+  doc.setFontSize(18);
+  doc.text("Dotlify Email Aliases", 14, 20);
+  
+  // Add content
+  doc.setFontSize(11);
+  
+  // Set starting position
+  let yPos = 30;
+  const pageHeight = doc.internal.pageSize.height;
+  
+  // Add each alias
+  aliases.forEach(alias => {
+    // Check if we need a new page
+    if (yPos > pageHeight - 20) {
+      doc.addPage();
+      yPos = 20;
+    }
+    
+    doc.text(alias, 14, yPos);
+    yPos += 7;
+  });
+  
+  // Save PDF
+  doc.save(`${this.filenameBase}.pdf`);
+};
